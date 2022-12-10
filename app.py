add5f907a762367dd76631b7406f5182d666c5c5
david-devito <55007717+david-devito@users.noreply.github.com>
2022-12-10 13:04:12 -0500

    Locked Left Column of DF

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from pullData import pullDataFromYahoo
 from st_aggrid import AgGrid
 from getSchedule import getTeamsPlayingOnSelectedDates
+from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 st.set_page_config(layout="wide")
 
@@ -31,9 +32,14 @@ curTeam = st.multiselect(
 
 filteredDF = most_added[most_added['Team'].isin(curTeam)]
 
-#st.dataframe(filteredDF)
 
-AgGrid(filteredDF, enable_enterprise_modules=False)
+gb = GridOptionsBuilder.from_dataframe(filteredDF)
+
+
+gb.configure_column('Name',pinned=True)
+gridOptions = gb.build()
+
+AgGrid(filteredDF, gridOptions=gridOptions, enable_enterprise_modules=True)
 
 
 
